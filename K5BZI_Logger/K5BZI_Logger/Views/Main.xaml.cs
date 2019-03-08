1ea967021cc4008f13be6cc4619e29d71919b4ae
Ray Eckel <raymondeckel@texasmutual.com>
2019-03-08 13:52:11 -0600

    removed viewmodel property from mainview code behind

--- K5BZI_Logger/K5BZI_Logger/Views/Main.xaml.cs
+++ K5BZI_Logger/K5BZI_Logger/Views/Main.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using K5BZI_Logger.Views.Interfaces;
-using K5BZI_Models.Main;
 using K5BZI_ViewModels.Interfaces;
 using MahApps.Metro.Controls;
-using System.Windows;
 
 namespace K5BZI_Logger.Views
 {
@@ -11,13 +9,11 @@ namespace K5BZI_Logger.Views
     /// </summary>
     public partial class Main : MetroWindow, IView
     {
-        private readonly MainModel _viewModel;
-
         public Main(IMainLoggerViewModel viewModel)
         {
             InitializeComponent();
 
-            DataContext = _viewModel = viewModel.Model;
+            DataContext = viewModel.Model;
         }
     }
 }
