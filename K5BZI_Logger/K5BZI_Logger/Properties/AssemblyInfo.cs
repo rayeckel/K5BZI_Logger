d4459d82345a5db9e4593aa8545a7024490aa9e8
Ray Eckel <raymondeckel@texasmutual.com>
2019-03-27 13:17:13 -0500

    Added icon and installer

--- K5BZI_Logger/K5BZI_Logger/Properties/AssemblyInfo.cs
+++ K5BZI_Logger/K5BZI_Logger/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("K5BZI_Logger")]
+[assembly: AssemblyTitle("K5BZI Logger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
