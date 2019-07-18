369048acca741b98bec5f336b071ce18d13fed47
reckel@truenorthlogic.com <reckel@truenorthlogic.com>
2019-07-18 11:25:59 -0500

    finished adding ARRL sections

--- K5BZI_Logger/K5BZI_Models/Enums/ArrlSectionsCanada.cs
+++ K5BZI_Logger/K5BZI_Models/Enums/ArrlSectionsCanada.cs
@@ -1,6 +1,32 @@
-﻿namespace K5BZI_Models.Enums
+﻿using System.ComponentModel;
+
+namespace K5BZI_Models.Enums
 {
     public enum ArrlSectionsCanada
     {
+        [Description("Maritime")]
+        MAR,
+        [Description("Newfoundland/Labrador")]
+        NL,
+        [Description("Quebec")]
+        QC,
+        [Description("Ontario North")]
+        ONN,
+        [Description("Ontario South")]
+        ONS,
+        [Description("Ontario East")]
+        ONE,
+        [Description("Greater Toronto Area")]
+        GTA,
+        [Description("Manitoba")]
+        MB,
+        [Description("Saskatchewan")]
+        SK,
+        [Description("Alberta")]
+        AB,
+        [Description("British Columbia")]
+        BC,
+        [Description("Northern Territories")]
+        NT
     }
 }
--- K5BZI_Logger/K5BZI_Models/Enums/ArrlSectionsMexico.cs
+++ K5BZI_Logger/K5BZI_Models/Enums/ArrlSectionsMexico.cs
@@ -1,6 +1,74 @@
-﻿namespace K5BZI_Models.Enums
+﻿using System.ComponentModel;
+
+namespace K5BZI_Models.Enums
 {
     public enum ArrlSectionsMexico
     {
+        //Used for ARRL 10m contest
+
+        [Description("Aguascalientes")]
+        AGS,
+        [Description("Baja California")]
+        BAC,
+        [Description("Baja California Sur")]
+        BCS,
+        [Description("Campiche")]
+        CAM,
+        [Description("Chaipas")]
+        CHI,
+        [Description("Chihuahua")]
+        CHH,
+        [Description("Coahuila")]
+        COA,
+        [Description("Colima")]
+        COL,
+        [Description("Distrito Federal")]
+        DFE,
+        [Description("Durango")]
+        DGO,
+        [Description("Estado de Mexico")]
+        EMX,
+        [Description("Guanajuato")]
+        GTO,
+        [Description("Guerrero")]
+        GRO,
+        [Description("Hidalgo")]
+        HGO,
+        [Description("Jalisco")]
+        JAL,
+        [Description("Michoacan")]
+        MIC,
+        [Description("Morelos")]
+        MOR,
+        [Description("Nayarit")]
+        NAY,
+        [Description("Nuevo Leon")]
+        NLE,
+        [Description("Oaxaca")]
+        OAX,
+        [Description("Puebla")]
+        PUE,
+        [Description("Queretaro")]
+        QRO,
+        [Description("Quintana Roo")]
+        QUI,
+        [Description("San Luis Potosi")]
+        SLP,
+        [Description("Sinaloa")]
+        SIN,
+        [Description("Sonroa")]
+        SON,
+        [Description("Tabasco")]
+        TAB,
+        [Description("Tamaulipas")]
+        TAM,
+        [Description("Tlaxcala")]
+        TLX,
+        [Description("Veracruz")]
+        VER,
+        [Description("Yucatan")]
+        YUC,
+        [Description("Zacatecas")]
+        ZAC
     }
 }
