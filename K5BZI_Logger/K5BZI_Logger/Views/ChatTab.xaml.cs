0419b238e4a7e2409f3e47d23389557c35f4e4ce
Ray Eckel <rayeckel@yahoo.com>
2023-11-30 00:41:42 -0600

    Added network tab

---
+++ K5BZI_Logger/K5BZI_Logger/Views/ChatTab.xaml.cs
@@ -0,0 +1,18 @@
+﻿using K5BZI_ViewModels.Interfaces;
+using MahApps.Metro.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace K5BZI_Logger.Views
+{
+    public partial class ChatTab : MetroTabItem
+    {
+        public ChatTab()
+        {
+            InitializeComponent();
+
+            DataContext = App.ServiceProvider
+                .GetRequiredService<INetworkViewModel>()
+                .NetworkModel;
+        }
+    }
+}
